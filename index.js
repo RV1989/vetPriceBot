2e4c398c33f24ba4bf0f6ecaeb62b9bbebfc88de
Rien Verbeke <rien__verbeke@msn.com>
2022-03-05 19:38:11 +0100

    env

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ const main = async () => {
 
   await sleep(1000);
   const twitterClient = new TwitterApi({
-    appKey: process.env.API_KEY,
-    appSecret: process.env.API_KEY_SECRET,
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_KEY_SECRET,
     // Following access tokens are not required if you are
     // at part 1 of user-auth process (ask for a request token)
     // or if you want a app-only client (see below)
-    accessToken: process.env.ACCESS_TOKEN,
-    accessSecret: process.env.ACCESS_TOKEN_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   });
   const mediaId = await twitterClient.v1.uploadMedia("./output/image.png");
   await twitterClient.v1.tweet(`#VeChain #VeFam $vet price: $ ${price}`, {
